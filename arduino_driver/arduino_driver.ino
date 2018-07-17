9ae31b7d2d127cd90869599f27c4b8fdf6acc3ea
John <johngodsey@gmail.com>
2018-07-17 21:23:31 +0000

    Adding features

--- arduino_driver/arduino_driver.ino
+++ arduino_driver/arduino_driver.ino
@@ -1,9 +1,61 @@
+const int dirA = 12;
+const int dirB = 13;
+
+const int pwmA = 3;
+const int pwmB = 11;
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  pinMode(dirA,OUTPUT);
+  pinMode(dirB,OUTPUT);
+  pinMode(pwmA,OUTPUT);
+  pinMode(pwmB,OUTPUT);
+  
 }
 
 void loop() {
+  if (Serial.available() > 0) {
+    String cmd = "";
+    cmd = Serial.readString();
+    Serial.println(cmd);
+
+    if (cmd == "FORWARD") {
+      digitalWrite(dirA, 1);
+      digitalWrite(dirB, 1);
+      digitalWrite(pwmA, 1);
+      digitalWrite(pwmB ,1);
+    }
+
+    if (cmd == "BACKWARD") {
+      digitalWrite(dirA, 0);
+      digitalWrite(dirB, 0);
+      digitalWrite(pwmA, 1);
+      digitalWrite(pwmB ,1);
+    }
+
+    if (cmd == "STOP") {
+      digitalWrite(pwmA, 0);
+      digitalWrite(pwmB, 0);
+    }
+
+    if (cmd == "LEFT") {
+      digitalWrite(dirA, 1);
+      digitalWrite(dirB, 0);
+      digitalWrite(pwmA, 1);
+      digitalWrite(pwmB ,1);
+    }
+
+    if (cmd == "RIGHT") {
+      digitalWrite(dirA, 0);
+      digitalWrite(dirB, 1);
+      digitalWrite(pwmA, 1);
+      digitalWrite(pwmB ,1);
+    }
+  }
+  
   // put your main code here, to run repeatedly:
   double val = (analogRead(2))/2;
   Serial.println(val);
--- index.js
+++ index.js
@@ -1,36 +1,74 @@
 
-var SerialPort = require('serialport');
-var parsers = SerialPort.parsers;
+	var SerialPort = require('serialport');
+	var parsers = SerialPort.parsers;
 
-var parser = new parsers.Readline({
-})
-var port = new SerialPort('/dev/ttyMCC', {
-	buadrate: 9600
-});
+	var parser = new parsers.Readline({
+	})
+	var port = new SerialPort('/dev/ttyMCC', {
+		buadrate: 9600
+	});
 
-port.pipe(parser);
+	port.pipe(parser);
 
-port.on('error', console.log);
-port.on('open', () => console.log('Port open'));
-parser.on('data', console.log);
+	port.on('error', console.log);
+	port.on('open', () => console.log('Port open'));
+	
+
+
 
 var neo = require("./udooneo")
 var sensors = neo.sensors;
 sensors.Accelerometer.enable();
-console.log(sensors.Accelerometer.data());
 
+var WebSocketClient = require('websocket').client;
+var http = require('http');
 
-//Motors
-var directionA = new neo.GPIO(28)
-var pwmA = new neo.GPIO(19)
+var client = new WebSocketClient();
+client.on('connectFailed', function(error) {
+	dead = true
+	console.log("Connect error: " + error.toString());
+			setTimeout(function() {
+			console.log("Reattempting connection");
+			client.connect('ws://192.168.1.70:8083/', 'robot');
+		}, 100);
+})
 
-var directionB = new neo.GPIO(29)
-var pwmB = new neo.GPIO(27)
+var dead = true;
+client.on('connect', function(connection) {
+	console.log('Connected to cloud');
+	dead = false
+	connection.on('error', function(error) {
+		console.log('Connection Error:' + error.toString());
+	});
 
-directionA.out();
-directionB.out();
-pwmA.out();
-pwmB.out();
+	connection.on('close', function() {
+		dead = true
+		console.log('robot connection closed');
+		setTimeout(function() {
+			console.log("Reattempting connection");
+			client.connect('ws://192.168.1.70:8083/', 'robot');
+		}, 100);
+	});
 
+	connection.on('message', function(message) {
+		console.log(message)
+		if (message.type === 'utf8') {
+			port.write(message.utf8Data, function(err) {
+				if (err != undefined) {
+					console.log(err)
+				}
+			});
+		}
+	});
+
+	parser.on('data', function(message) {
+		if (dead == false) {
+			connection.sendUTF("FromArduino:" + message);
+			connection.sendUTF("FromUdoo:" +sensors.Accelerometer.data());
+		}
+	});
+});
+
+client.connect('ws://192.168.1.70:8083/', 'robot');
 
 
